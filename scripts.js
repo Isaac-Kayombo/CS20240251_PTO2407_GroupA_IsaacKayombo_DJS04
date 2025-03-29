9addf46c160589b9745be35692fbfc57dcfb618a
IsaacK47 <isaackayombo41@gmail.com>
2025-03-29 14:24:42 +0200

    function to render book preview

--- scripts.js
+++ scripts.js
@@ -3,29 +3,24 @@ import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 let page = 1;
 let matches = books
 
-const starting = document.createDocumentFragment()
+// CREATING HELPER FUNCTIONS FOR REUSABLE CODE
 
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
+// FUNCTION TO RENDER BOOK PREVIEW
+function renderBookPreview(book) {
+    const element = document.createElement("button");
+    element.classList = "preview";
+    element.setAttribute("data-preview", book.id);
 
     element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
+        <img class="preview__image" src="${book.image}" />
         <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
+            <h3 class="preview__title">${book.title}</h3>
+            <div class="preview__author">${authors[book.author]}</div>
         </div>
-    `
+    `;
 
-    starting.appendChild(element)
+    return element;
 }
-
-document.querySelector('[data-list-items]').appendChild(starting)
 
 const genreHtml = document.createDocumentFragment()
 const firstGenreElement = document.createElement('option')
