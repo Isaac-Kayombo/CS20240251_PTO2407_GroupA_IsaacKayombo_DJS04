73942810a65cea9cfab7c5bdb2d1dd560088dddf
IsaacK47 <isaackayombo41@gmail.com>
2025-03-29 14:50:40 +0200

    function for theme settings

--- scripts.js
+++ scripts.js
@@ -67,15 +67,20 @@ function renderAuthors() {
     document.querySelector('[data-search-authors]').appendChild(authorsHtml);
 }
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.querySelector('[data-settings-theme]').value = 'night'
-    document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
-    document.documentElement.style.setProperty('--color-light', '10, 10, 20');
-} else {
-    document.querySelector('[data-settings-theme]').value = 'day'
-    document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
-    document.documentElement.style.setProperty('--color-light', '255, 255, 255');
+// FUNCTION TO HANDLE THEME SETTING BASED ON USER SELECTION
+function handleTheme() {
+    const themeSetting = document.querySelector('[data-setting-theme]');
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        themeSetting.value = 'night'
+        document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
+        document.documentElement.style.setProperty('--color-light', '10, 10, 20');
+    } else {
+        themeSetting.value = 'day'
+        document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
+        document.documentElement.style.setProperty('--color-light', '255, 255, 255');
+    }
 }
+
 
 document.querySelector('[data-list-button]').innerText = `Show more (${books.length - BOOKS_PER_PAGE})`
 document.querySelector('[data-list-button]').disabled = (matches.length - (page * BOOKS_PER_PAGE)) > 0
