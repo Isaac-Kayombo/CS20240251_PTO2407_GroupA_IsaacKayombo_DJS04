1721cf5e9432a3ebc26ec13f03f34a6718592b0c
IsaacK47 <isaackayombo41@gmail.com>
2025-03-29 14:35:35 +0200

    function to render genres dropdown

--- scripts.js
+++ scripts.js
@@ -31,12 +31,24 @@ function renderBookList(bookArray) {
     document.querySelector('[data-list-items]').appendChild(fragment);
 }
 
+// FUNCTION TO RENDER GENRES 
+function renderGenres() {
+    const genreHtml = document.createDocumentFragment();
+    const firstGenreElement = document.createElement('option');
+    firstGenreElement.value = 'any';
+    firstGenreElement.innerText = 'All Genres';
+    genreHtml.appendChild(firstGenreElement);
 
-const genreHtml = document.createDocumentFragment()
-const firstGenreElement = document.createElement('option')
-firstGenreElement.value = 'any'
-firstGenreElement.innerText = 'All Genres'
-genreHtml.appendChild(firstGenreElement)
+    for (const [id, name] of Object.entries(genres)) {
+        const element = document.createElement('option');
+        element.value = id;
+        element.innerText = name;
+        genreHtml.appendChild(element);
+    }
+
+    document.querySelector('[data-search-genres]').appendChild(genreHtml);
+}
+
 
 for (const [id, name] of Object.entries(genres)) {
     const element = document.createElement('option')
